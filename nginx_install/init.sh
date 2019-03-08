32011a2ec3dda4924fcc6bf20041919c77758ccc
Pavel <wabloxa@yandex.ru>
2019-03-08 17:46:44 +0300

    Regexp modified

--- nginx_install/init.sh
+++ nginx_install/init.sh
@@ -1,3 +1,12 @@
-sudo mv /home/box/web/nginx_install/* /home/box/web/
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#!/bin/bash
+
+# Expected that this repository git cloned to /home/box/web/
+
+# 1. Move all files from cloned repository to /home/box/web
+sudo mv /home/box/web/webtechcourse/nginx_install/* /home/box/web/
+
+# 2. Make symbolic link to configuration && rewrite default conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+
+# 3. Restart nginx daemon
 sudo /etc/init.d/nginx restart
